7b1774a06b237cd937e8ee16348b3f5cc23ed085
Maksym <maxim4110@gmail.com>
2020-01-30 10:46:54 +0200

    responsive fix, gallery new images

---
+++ lab06/main.js
@@ -0,0 +1,12 @@
+function wordLength(s, n) {
+  const words = s.split(" ")
+  return words.length >= n - 1 ? words[n] : null
+}
+
+function harmonicMean(x1, x2, x3) {
+  return 3 / (1 / x1 + 1 / x2 + 1 / x3)
+}
+
+function taylorSeries(x, n) {
+
+}
